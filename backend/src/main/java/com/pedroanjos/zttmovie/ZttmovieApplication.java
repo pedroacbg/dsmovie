2ef0e6e0bb77615ee6a607a050cff104649fa665
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-03-09 11:06:03 -0300

    Project created

---
+++ backend/src/main/java/com/pedroanjos/zttmovie/ZttmovieApplication.java
@@ -0,0 +1,13 @@
+package com.pedroanjos.zttmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZttmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZttmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pedroanjos/zttmovie/ZttmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroanjos.zttmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZttmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
